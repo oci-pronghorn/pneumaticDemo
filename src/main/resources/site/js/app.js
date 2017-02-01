b3c17bdd47a52dbe108c0a85893cf07a0bd4a937
Nathan Tippy <nathan@tippy.name>
2017-02-01 09:37:36 -0600

    cleanup and removal of old comments

--- src/main/resources/site/js/app.js
+++ src/main/resources/site/js/app.js
@@ -202,46 +202,7 @@ app.controller('MyCtrl', (function($scope) {
 	  
   	  
 	  startupWebSocket();
-	  
-      //need one of these per canvas
-    //  const smoothie = [new SmoothieChart({millisPerPixel:40,maxValue:160,minValue:-40,interpolation:'linear'}),
-	//			        new SmoothieChart({millisPerPixel:40,interpolation:'linear'}),
-	//			        new SmoothieChart({millisPerPixel:40,interpolation:'linear'}),
-	//			        new SmoothieChart({millisPerPixel:40,maxValue:100000000,minValue:0,labels:{disabled:true}})]
-				   			            
-	// smoothie[0].streamTo(document.getElementById("canvas"+0));		  
-	// smoothie[1].streamTo(document.getElementById("canvas"+1));	
-	// smoothie[2].streamTo(document.getElementById("canvas"+2));	
-	// smoothie[3].streamTo(document.getElementById("canvas"+3),250);	 
-	
-    //  const line = [new TimeSeries(), //0sine
-	//                new TimeSeries(), //1random
-	//                new TimeSeries(), //2system cpu
-	//                new TimeSeries(), //3process cpu
-	//                new TimeSeries(), //4motion
-	//                new TimeSeries(), //5light
-	//                new TimeSeries(), //6uv
-	//                new TimeSeries(), //7mosit
-//	                new TimeSeries(), //8button 
-	//                new TimeSeries()  //9rotery
-	 //               ]
-                								
-			
-	 /// smoothie[0].addTimeSeries(line[1]);
-	 // smoothie[0].addTimeSeries(line[0],{ strokeStyle:'rgb(0, 255, 0)', fillStyle:'rgba(0, 255, 0, 0.4)', lineWidth:2 });
-
-    //  smoothie[1].addTimeSeries(line[5],{ strokeStyle:'rgb(255, 255, 255)', fillStyle:'rgba(255, 255, 255, 0.4)', lineWidth:2 }); //light
-    //  smoothie[1].addTimeSeries(line[6],{ strokeStyle:'rgb(255, 0, 255)', fillStyle:'rgba(255, 0, 255, 0.4)', lineWidth:2 }); //uv
-    //  smoothie[1].addTimeSeries(line[7],{ strokeStyle:'rgb(165,42,42)', fillStyle:'rgba(165,42,42, 0.4)', lineWidth:2 }); //mositure
-      
-      
-    //  smoothie[2].addTimeSeries(line[4],{ strokeStyle:'rgb(255, 255, 255)', fillStyle:'rgba(255, 255, 255, 0.4)', lineWidth:2 }); //motion -- moving average.
-    //  smoothie[2].addTimeSeries(line[8],{ strokeStyle:'rgb(255, 0, 0)', fillStyle:'rgba(255, 0, 0, 0.4)', lineWidth:2 }); //button -- spike
-    //  smoothie[2].addTimeSeries(line[9],{ strokeStyle:'rgb(0, 0, 255)', fillStyle:'rgba(0, 0, 255, 0.4)', lineWidth:2 }); //rotary --must bound turn up and down
-      
-
-	//  smoothie[3].addTimeSeries(line[3],{ strokeStyle:'rgb(0, 255, 0)', fillStyle:'rgba(0, 255, 0, 0.4)', lineWidth:2 });
-	//  smoothie[3].addTimeSeries(line[2],{ strokeStyle:'rgb(255, 0, 0)', fillStyle:'rgba(255, 0, 0, 0.4)', lineWidth:2 });//on top last
+	 
 
 		$scope.send = function(message) {
 		}		
@@ -254,94 +215,7 @@ app.controller('MyCtrl', (function($scope) {
 		
 		
 	  $scope.valve = [{},{},{},{}]; 
-		
-		/////////////////////////////////////
-		//example code using Graphvis to build SVG
-		////////////////////////////////////////
-		
-	/////////////////////////////////////////////////////	
-		//example code using graphiz svg
-			      // if (segments[3] == "LeakFault") {
-	      	  // if (message.payloadString == "True") {
-	      	  	// $scope.stationStyles["m1s1"]="style=filled, fillcolor=blue";
-	      	  // } else {
-	      	  	// $scope.stationStyles["m1s1"]="";
-	      	  // }
-	      // }
-// 	      
-	      // $scope.result = buildSVG(buildFactory($scope.stationStyles));	      	      
-	      // $scope.$apply();
-// 	/////////////////////////////////////////////////////////////	
-		
-		
-		
-	// function buildStationStyle(manifold, station, label, stationStyles) {
-// 		
-			// var style = stationStyles[station];
-			// return manifold.concat("->",station, ";\n",station,"[label=\"",label,"\"",style,"];\n");
-// 
-	// }	
-// 
-	// // style=filled, fillcolor=red
-// 	
-	// $scope.stationStyles ={ 
-	                     	// "m1s1" : "",
-	                        // "m1s2" : "",
-	                        // "m1s3" : "",
-// 	                        
-	                        // "m2s1" : "",
-	                        // "m2s2" : "",
-	                        // "m2s3" : "",
-	                        // "m2s4" : "",
-	                        // "m2s5" : "",
-// 	                     
-	                        // "m3s1" : "",
-	                        // "m3s2" : "",
-// 	                        
-	                        // };
-// 		
-	// function buildFactory(stationStyles) {
-// 	
-		// var result= "digraph {\n";
-// 		
-		// result = result.concat("rankdir = LR\n");
-// 		
-		// result = result.concat("Compressor->m1"," [penwidth=3];\n");		
-		// result = result.concat("m1->m2"," [penwidth=3];\n");
-		// result = result.concat("m2->m3"," [penwidth=3];\n");
-// 		
-		// result = result.concat("{ rank=same Compressor, m1, m2, m3 }\n");
-// 		
-		// result = result.concat("m1 [style=bold,label=\"Manifold 1\"]\n");
-		// result = result.concat("m2 [style=bold,label=\"Manifold 2\"]\n");
-		// result = result.concat("m3 [style=bold,label=\"Manifold 3\"]\n");
-// 		
-	    // result = result.concat(buildStationStyle("m1","m1s1","Station 11",stationStyles));
-	    // result = result.concat(buildStationStyle("m1","m1s2","Station 12",stationStyles));
-	    // result = result.concat(buildStationStyle("m1","m1s3","Station 13",stationStyles));
-// 	    
-	    // result = result.concat(buildStationStyle("m2","m2s1","Station 21",stationStyles));
-	    // result = result.concat(buildStationStyle("m2","m2s2","Station 22",stationStyles));
-	    // result = result.concat(buildStationStyle("m2","m2s3","Station 23",stationStyles));
-	    // result = result.concat(buildStationStyle("m2","m2s4","Station 24",stationStyles));
-	    // result = result.concat(buildStationStyle("m2","m2s5","Station 25",stationStyles));
-// 	    
-	    // result = result.concat(buildStationStyle("m3","m3s1","Station 31",stationStyles));
-	    // result = result.concat(buildStationStyle("m3","m3s2","Station 32",stationStyles));
-// 
-// 
-		// result=result.concat("}");			
-		// return result;
-// 		
-	// }	
-	// function buildSVG(dot) {
-		// return Viz(dot,{format: "svg", engine: "dot"})
-	  // .replace("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>","");
-	// }
-		
-		/////////////////////////////////////
-		//end of graphviz example code
-		//////////////////////////////////
+
 		
 	function circleRun(fills, cx1, cy1, stepX, stepY, r ) {		
 		 var c;
@@ -353,21 +227,34 @@ app.controller('MyCtrl', (function($scope) {
 		
 		return result;
 	}	
-	
-	//TODO: manidfold square?
-	//      draw booth?
-		
-		
+
 	function circle(fill, cx, cy, r) {
-		return "<circle ng-click=\"alert('zzzz');\"   fill=\"".concat(fill,"\" stroke=\"black\" cx=\"",cx,"\" cy=\"",cy,"\" r=\"", r,"\"/>"); 
+		return "<circle ng-click=\"console.log('zzzz');\"   fill=\"".concat(fill,"\" stroke=\"black\" cx=\"",cx,"\" cy=\"",cy,"\" r=\"", r,"\"/>"); 
 	}	
 		
 		
-	function test() {
-		scope.result = "";
-		$scope.$apply();
-		 alert("yyyy");
+	$scope.leakFilter = function() {
+		console.log("hello");
 	}	
+	
+	$scope.valveFilter = function() {
+		console.log("hello");
+	}			
+		
+	$scope.overFilter = function() {
+		console.log("hello");
+	}
+	
+	$scope.underFilter = function() {
+		console.log("hello");
+	}		
+		
+	$scope.lifeFilter = function(limit) {
+		console.log("hello "+limit);
+	}		
+	
+	$scope.lifeLimit = 1000;
+	
 		
 	$scope.m1ValveStates = 	["none","red"];
 	$scope.m2ValveStates = 	["none","red","none"];
@@ -381,12 +268,16 @@ app.controller('MyCtrl', (function($scope) {
 		var result = "";
 		
 		var xBase = 20;
+		var yBase = 150;
 		
-		result = result.concat(circleRun($scope.m1ValveStates, xBase+0, -400, 20, -10, 5));
-		result = result.concat(circleRun($scope.m2ValveStates, xBase+0, -200, 20, 10, 5));
-		result = result.concat(circleRun($scope.m3ValveStates, xBase+370, -210, -20, 10, 5));
-		result = result.concat(circleRun($scope.m4ValveStates, xBase+0, -210, 10, -30, 5));
-		result = result.concat(circleRun($scope.m3ValveStates, xBase+400, -270, -10, 30, 5));
+		result = result.concat(circleRun($scope.m1ValveStates, xBase+10, yBase-400, 20, -10, 5));
+		result = result.concat(circleRun($scope.m4ValveStates, xBase+10, yBase-230, 0, -30, 5));
+		result = result.concat(circleRun($scope.m2ValveStates, xBase+20, yBase-190, 20, 10, 5));
+		
+		result = result.concat(circleRun($scope.m3ValveStates, xBase+350, yBase-190, -20, 10, 5));
+		result = result.concat(circleRun($scope.m3ValveStates, xBase+370, yBase-310, 0, 30, 5));
+		
+		//result = result.concat(circle("blue",0,0,10) );
 		
 		return result;
 	}	
@@ -394,9 +285,10 @@ app.controller('MyCtrl', (function($scope) {
 		  
 	function buildSVGFloor() {
 				
+		console.log("zzzzzzzzzzzzzzzzzzzzz  hello");
 		
-	   return "<svg width=\"640pt\" height=\"530pt\" viewBox=\"0.00 0.00 640 530\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"> \
-	          <g id=\"graph0\" class=\"graph\" transform=\"scale(1 1) rotate(0) translate(4 526) \">".concat(mapItems(),
+	   return "<svg width=\"640pt\" height=\"380pt\" viewBox=\"0.00 0.00 640 380\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"> \
+	          <g id=\"graph0\" class=\"graph\" transform=\"scale(1 1) rotate(0) translate(4 380) \">".concat(mapItems(),
 	          "</g>  </svg>");
 			  
 
